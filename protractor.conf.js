6941d35b2afa9598e6df0e2c4b9ccd2f87fe3a6c
Lida Borisenko <borilig@gmail.com>
2018-02-12 21:53:01 +0200

    protractor.conf.js file edited

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,3 +1,8 @@
+require("ts-node").register();
 
 module.exports.config = {
+  specs: ['search.ts' , /*'spec.ts',*/  'navigation.ts' , "movieCArd.ts" ],
+  directConnect: true,
+  baseUrl: "https://movies-finder.firebaseapp.com/",
   SELENIUM_PROMISE_MANAGER: false
+};
