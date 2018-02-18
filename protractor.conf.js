62fb30a9c8479b844d9733d6fb0169c12100673b
Lida Borisenko <borilig@gmail.com>
2018-02-18 19:06:04 +0200

    Lesson5task

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,7 @@
 require("ts-node").register();
 
 module.exports.config = {
-  specs: ['search.ts' , /*'spec.ts',*/  'navigation.ts' , "movieCArd.ts" ],
+  specs: ['lesson5task.ts' /*'search.ts' ,'spec.ts',  'navigation.ts' , "movieCArd.ts" */],
   directConnect: true,
   baseUrl: "https://movies-finder.firebaseapp.com/",
   SELENIUM_PROMISE_MANAGER: false
