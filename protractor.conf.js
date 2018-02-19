11b6a174c20e8bae9330d9824afea2c8d0cfb93f
Lida Borisenko <borilig@gmail.com>
2018-02-19 22:44:47 +0200

    for TravisCI integration

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,11 @@ require("ts-node").register();
 
 module.exports.config = {
   specs: ['lesson5task.ts' /*'search.ts' ,'spec.ts',  'navigation.ts' , "movieCArd.ts" */],
-  directConnect: true,
   baseUrl: "https://movies-finder.firebaseapp.com/",
-  SELENIUM_PROMISE_MANAGER: false
+  SELENIUM_PROMISE_MANAGER: false,
+  capabilities: { 
+  browserName: 'chrome', 
+  enableVNC: true,
+  name: "YOUR NAME HERE" // Just to identify your session between others on selenoid ui
+},
 };
